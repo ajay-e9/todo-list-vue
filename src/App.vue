d9f1c11281519f0f3886febd758f6e9015b524fe
Ajay Kumar Gupta <ajay.gupta@e9.co.uk>
2023-09-19 16:22:58 +0530

    chore: tailwind init

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<template></template>
+<template>
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+</template>
 
 <style scoped>
 </style>
